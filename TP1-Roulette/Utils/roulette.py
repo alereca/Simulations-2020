a3e1bf41da80096020c4b11bae04334e99df48bc
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-04-03 20:46:45 -0300

    Test without using numpy

--- TP1-Roulette/Utils/roulette.py
+++ TP1-Roulette/Utils/roulette.py
@@ -1,8 +1,22 @@
 import random as rand
 
+
 def generate_array_of_rf(selected_num, num_iters):
+    matrix = [[], []]
     occurrences = 0
-    for num_of_iteration in range(1, num_iters):
-        if selected_num == rand.randint(0, 36):
+    last_avg = 0
+    last_variance = 0
+    for i in range(1, num_iters):
+        random_int = rand.randint(0, 36)
+        if selected_num == random_int:
             occurrences += 1
-        yield occurrences / num_of_iteration
+
+        actual_rf = occurrences / i
+        avg = ((last_avg * (i-1)) + random_int) / i
+        last_variance = (random_int - avg)
+
+        matrix[0].append(actual_rf)
+        matrix[1].append(avg)
+        last_avg = avg
+
+    return matrix
--- TP1-Roulette/__init__.py
+++ TP1-Roulette/__init__.py
@@ -3,10 +3,10 @@ from Graphs.line_plot import plot_graph
 
 selected_num = 21
 num_iters = 1500
-expected_rf = 1 / 37
+expected_rf = 18
 
 if __name__ == "__main__":
-    rf_list = list(generate_array_of_rf(
-        selected_num, num_iters))
-    print(rf_list)
-    plot_graph(rf_list, num_iters, selected_num, expected_rf)
+    rf_list = generate_array_of_rf(
+        selected_num, num_iters)
+    print(rf_list[1])
+    plot_graph(rf_list[1], num_iters, selected_num, expected_rf)
