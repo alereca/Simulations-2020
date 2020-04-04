d2fe1125e15d927bfe1df04c3e98303469d44d55
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-04-04 18:03:01 -0300

    Discard single merged array idea and add multiple array values display

--- TP1-Roulette/Graphs/subplot.py
+++ TP1-Roulette/Graphs/subplot.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 
-def general_generate_subplot(figure, x_axis, n_iter):
-    def specific_generate(metrics_array, expected_value, params):
+def general_generate_subplot(figure, x_axis, n_iters):
+    def specific_generate(metrics_arrays, expected_value, params):
         ax = figure.add_subplot(2, 2, params.position)
-        ax.plot(x_axis, metrics_array)
-        ax.hlines(expected_value, 0, n_iter,
+        for metrics_array in metrics_arrays:
+            ax.plot(x_axis, metrics_array)
+        ax.hlines(expected_value, 0, n_iters,
                   colors="red", linestyles="dashed")
         ax.set_title(label=params.title)
         ax.set_xlabel(xlabel=params.xlabel)
--- TP1-Roulette/__init__.py
+++ TP1-Roulette/__init__.py
@@ -1,9 +1,8 @@
 from Utils.single_roulette_metrics import generate_multi_metrics_array
-from Utils.multiple_roulette_metrics import merge_multi_metric_arrays
 from Graphs.line_plot import plot_graph
 
 selected_num = 7
-num_iters = 1000
+num_iters = 10000
 expected_values = {
     "rf": 1 / 37,
     "avg": 18,
@@ -12,11 +11,23 @@ expected_values = {
 }
 
 if __name__ == "__main__":
-    """ metrics_array = generate_multi_metrics_array(
-        selected_num, num_iters) """
-    merged_multi_metrics_array = merge_multi_metric_arrays(num_iters, [
-        generate_multi_metrics_array(7, num_iters),
-        generate_multi_metrics_array(7, num_iters),
-        generate_multi_metrics_array(7, num_iters)
-    ])
-    plot_graph(merged_multi_metrics_array, num_iters, selected_num, expected_values)
+    plot_graph(
+        n_iters=num_iters,
+        num=selected_num,
+        expected_values=expected_values,
+        multi_metrics_arrays=[
+            generate_multi_metrics_array(selected_num, num_iters)
+        ],
+        file_out="single_run_roulette_metrics.jpg"
+    )
+    plot_graph(
+        n_iters=num_iters,
+        num=selected_num,
+        expected_values=expected_values,
+        multi_metrics_arrays=[
+            generate_multi_metrics_array(selected_num, num_iters),
+            generate_multi_metrics_array(selected_num, num_iters),
+            generate_multi_metrics_array(selected_num, num_iters)
+        ],
+        file_out="multi_run_roulette_metrics.jpg"
+    )
