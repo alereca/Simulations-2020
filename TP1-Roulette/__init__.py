4445ab6e4fd27a1fc76c739c7bb65bb07b0a4805
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-04-07 19:43:53 -0300

    Change to multiple random numbers populations approach

--- TP1-Roulette/__init__.py
+++ TP1-Roulette/__init__.py
@@ -1,13 +1,14 @@
-from Utils.single_roulette_metrics import generate_multi_metrics_array
+from Utils.random_numbers_populations import generate_random_numbers_populations
+from Utils.roulette_metrics import generate_multi_metrics_array
 from Graphs.line_plot import plot_graph
 
 selected_num = 7
-num_iters = 30
+num_iters = 10000
 expected_values = {
     "rf": 1 / 37,
     "avg": 18,
-    "std": 10.67,
-    "var": 10.67 ** 2
+    "std": 114**(1/2),
+    "var": 114
 }
 
 if __name__ == "__main__":
@@ -16,7 +17,10 @@ if __name__ == "__main__":
         num=selected_num,
         expected_values=expected_values,
         multi_metrics_arrays=[
-            generate_multi_metrics_array(selected_num, num_iters)
+            generate_multi_metrics_array(
+                generate_random_numbers_populations(num_iters),
+                selected_num, num_iters
+            )
         ],
         file_out="single_run_roulette_metrics.jpg"
     )
@@ -25,12 +29,30 @@ if __name__ == "__main__":
         num=selected_num,
         expected_values=expected_values,
         multi_metrics_arrays=[
-            generate_multi_metrics_array(selected_num, num_iters),
-            generate_multi_metrics_array(selected_num, num_iters),
-            generate_multi_metrics_array(selected_num, num_iters),
-            generate_multi_metrics_array(selected_num, num_iters),
-            generate_multi_metrics_array(selected_num, num_iters),
-            generate_multi_metrics_array(selected_num, num_iters)
+            generate_multi_metrics_array(
+                generate_random_numbers_populations(num_iters),
+                selected_num, num_iters
+            ),
+            generate_multi_metrics_array(
+                generate_random_numbers_populations(num_iters),
+                selected_num, num_iters
+            ),
+            generate_multi_metrics_array(
+                generate_random_numbers_populations(num_iters),
+                selected_num, num_iters
+            ),
+            generate_multi_metrics_array(
+                generate_random_numbers_populations(num_iters),
+                selected_num, num_iters
+            ),
+            generate_multi_metrics_array(
+                generate_random_numbers_populations(num_iters),
+                selected_num, num_iters
+            ),
+            generate_multi_metrics_array(
+                generate_random_numbers_populations(num_iters),
+                selected_num, num_iters
+            )
         ],
         file_out="multi_run_roulette_metrics.jpg"
     )
