ceeca646130094d75797ae9be324549bcf7cc69e
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-04-23 20:14:05 -0300

    Add board numbers and check color in martingale strategy

--- TP1.2_Bet/Strategies/martingale_strategy.py
+++ TP1.2_Bet/Strategies/martingale_strategy.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def martingale_strategy(capital, bet_amount, bet_number):
+def martingale_strategy(board, capital, bet_amount, color):
     historic_capital_array = []
     frequency_array = []
     victories_acum = 0
@@ -9,16 +9,14 @@ def martingale_strategy(capital, bet_amount, bet_number):
 
     while capital > bet_amount:
         random_roulette_number = randint(0, 36)
-        if random_roulette_number == bet_number:
+        if board[random_roulette_number].color == color:
             capital += bet_amount * 36
             victories_acum += 1
-        if random_roulette_number != bet_number:
+        if random_roulette_number != color:
             capital -= bet_amount
             bet_amount += bet_amount * 2
             defeats_acum += 1
         historic_capital_array.append(capital)
-        frequency_array.append(
-            victories_acum / (victories_acum + defeats_acum)
-        )
+        frequency_array.append(victories_acum / (victories_acum + defeats_acum))
 
     return {"frequency": frequency_array, "capital": historic_capital_array}
--- TP1.2_Bet/__init__.py
+++ TP1.2_Bet/__init__.py
@@ -1,5 +1,6 @@
 from Strategies.martingale_strategy import martingale_strategy
+from Board.board import generate_board
 
 if __name__ == "__main__":
-    results = martingale_strategy(150000, 5, 7)
+    results = martingale_strategy(generate_board(), 150000, 5, "red")
     print(results)
