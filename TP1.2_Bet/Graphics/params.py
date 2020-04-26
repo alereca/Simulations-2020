42ff07b69564304f95d2885af5a0494dee63e420
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-04-26 13:22:08 -0300

    Update graphic generation
    Generalize it using config arguments

---
+++ TP1.2_Bet/Graphics/params.py
@@ -0,0 +1,6 @@
+class Params:
+    def __init__(self, title, xlabel, position, ylabel=""):
+        self.title = title
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.position = position
--- TP1.2_Bet/__init__.py
+++ TP1.2_Bet/__init__.py
@@ -1,8 +1,42 @@
-from Strategies.martingale_strategy import obtain_results
-from Graphics.martingale_strategy_graphic import graphics
-import seaborn as sns
+from Strategies.martingale_strategy import martingale_strategy
+from Board.board import generate_board
+from Graphics.generic_strategy_graphic import generate_graphic
+
 
 if __name__ == "__main__":
-    m_s_results = obtain_results()
-    sns.set()
-    graphics(m_s_results)
+    results = {
+        "limited_capital": [
+            martingale_strategy(
+                generate_board(),
+                initial_capital=15000,
+                initial_bet_amount=5,
+                color="red",
+            ),
+            martingale_strategy(
+                generate_board(),
+                initial_capital=15000,
+                initial_bet_amount=5,
+                color="black",
+            ),
+        ],
+        "unlimited_capital": [
+            martingale_strategy(
+                generate_board(),
+                initial_capital=15000,
+                initial_bet_amount=5,
+                color="red",
+                unlimited_money=True,
+                max_iterations=1000,
+            ),
+            martingale_strategy(
+                generate_board(),
+                initial_capital=15000,
+                initial_bet_amount=5,
+                color="black",
+                unlimited_money=True,
+                max_iterations=1000,
+            ),
+        ],
+    }
+
+    generate_graphic(results)
