c0fb04fb5ec1e39c457e5e6bde35bc7e8e194e39
olivamartin <49007030+olivamartin@users.noreply.github.com>
2020-05-20 13:53:13 -0300

    Add files via upload

--- TP1.2_Bet/Strategies/martingale_strategy.py
+++ TP1.2_Bet/Strategies/martingale_strategy.py
@@ -10,22 +10,22 @@ def martingale_strategy(config, board):
     iters = 0
     capital = config.initial_capital
     bet_amount = config.initial_bet_amount
-    unlimited_capital = config.max_iterations is not None
+    unlimited_capital = config.unlimited_capital
 
     historic_capital_array.append(config.initial_capital)
     frequency_array.append(0)
-    while (unlimited_capital is False and capital > bet_amount) or (
+    while (unlimited_capital is False and capital > bet_amount and capital > 0 and iters < config.max_iterations - 1) or (
         unlimited_capital and iters < config.max_iterations - 1
     ):
         iters += 1
         board_num = board[randint(0, 36)]
         if board_num.color == config.color:
-            capital += bet_amount * 2
+            capital += abs(bet_amount * 2)
             bet_amount = config.initial_bet_amount
             victories_acum += 1
         if board_num.color != config.color:
-            capital -= bet_amount
-            bet_amount = bet_amount * 2
+            capital -= abs(bet_amount)
+            bet_amount = abs(bet_amount * 2)
             defeats_acum += 1
         frequency_array.append(victories_acum / (victories_acum + defeats_acum))
         historic_capital_array.append(capital)
