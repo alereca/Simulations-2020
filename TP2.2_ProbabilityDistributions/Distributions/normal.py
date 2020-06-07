15fb4b0b04fe640c9eced14dc1df4a41814c4d7c
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-06-07 13:33:52 -0300

    Add normal distribution

---
+++ TP2.2_ProbabilityDistributions/Distributions/normal.py
@@ -0,0 +1,19 @@
+import numpy as np
+from Utils.generic_test import generic_test
+
+
+def normal_num(mean, std):
+    acum = 0
+    for _ in range(12):
+        r = np.random.uniform()
+        acum += r
+    return std * (acum - 6) + mean
+
+
+def test_normal(length, mean, std):
+    generic_test(
+        test_name=f"Normal(mean: {mean}| std: {std}| length: {length})",
+        sequence=np.array([normal_num(mean, std) for _ in range(length)]),
+        expected_mean=mean,
+        expected_var=std ** 2
+    )
--- TP2.2_ProbabilityDistributions/__init__.py
+++ TP2.2_ProbabilityDistributions/__init__.py
@@ -1,8 +1,10 @@
 from Distributions.exponential import test_exponential
 from Distributions.pascal import test_pascal
+from Distributions.normal import test_normal
 
 if __name__ == "__main__":
     length = 1000
 
     test_exponential(length, lmda=35)
     test_pascal(length, k=50, q=0.40)
+    test_normal(length, mean=20, std=2)
