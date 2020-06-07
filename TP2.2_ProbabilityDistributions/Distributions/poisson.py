e130078b8e3674cc40156ce801fb907b0cf8998e
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-06-07 15:59:01 -0300

    Add poisson distribution

---
+++ TP2.2_ProbabilityDistributions/Distributions/poisson.py
@@ -0,0 +1,28 @@
+import numpy as np
+import math
+from Utils.generic_test import generic_test
+
+
+def poisson_num(lmda):
+    multiplicative_series = 1
+    e_minus_lmda = math.exp(-lmda)
+    x = 0
+    while True:
+        r = np.random.uniform()
+        multiplicative_series *= r
+
+        if multiplicative_series > e_minus_lmda:
+            x += 1
+        else:
+            break
+
+    return x
+
+
+def test_poisson(length, lmda):
+    generic_test(
+        test_name="Poisson (lmda: {lmbda}| length: {length}",
+        sequence=np.array([poisson_num(lmda) for _ in range(length)]),
+        expected_mean=lmda,
+        expected_var=lmda,
+    )
--- TP2.2_ProbabilityDistributions/__init__.py
+++ TP2.2_ProbabilityDistributions/__init__.py
@@ -1,6 +1,7 @@
 from Distributions.exponential import test_exponential
 from Distributions.pascal import test_pascal
 from Distributions.normal import test_normal
+from Distributions.poisson import test_poisson
 
 if __name__ == "__main__":
     length = 1000
@@ -8,3 +9,4 @@ if __name__ == "__main__":
     test_exponential(length, lmda=35)
     test_pascal(length, k=50, q=0.40)
     test_normal(length, mean=20, std=2)
+    test_poisson(length, lmda=1)
