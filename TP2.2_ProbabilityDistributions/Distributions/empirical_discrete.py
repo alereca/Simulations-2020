4f04845fa65999a667ecedd93c02d8726fd9ccee
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-06-09 18:13:53 -0300

    Use probability array in empirical distribution

--- TP2.2_ProbabilityDistributions/Distributions/empirical_discrete.py
+++ TP2.2_ProbabilityDistributions/Distributions/empirical_discrete.py
@@ -1,22 +1,23 @@
 import numpy as np
+import functools
 
+def closest(lst, K):
+    return lst[min(range(len(lst)), key=lambda i: abs(lst[i] - K))]
 
 def empirical_discrete_num():
     r = np.random.uniform()
 
-    if 0 <= r < 0.1:
-        return 2
-    elif 0.1 <= r < 0.2:
-        return 3
-    elif 0.2 <= r < 0.35:
-        return 7
-    elif 0.35 <= r < 0.50:
-        return 6
-    elif 0.5 <= r < 0.7:
-        return 9
-    elif 0.7 <= r < 0.8:
-        return 4
-    elif 0.8 <= r < 0.95:
-        return 1
-    elif 0.95 <= r <= 1:
-        return 0
+    distribution = {
+        1: [0.273, 0.273],
+        2: [0.037, 0.31],
+        3: [0.195, 0.505],
+        4: [0.009, 0.514],
+        5: [0.124, 0.638],
+        6: [0.058, 0.696],
+        7: [0.062, 0.758],
+        8: [0.151, 0.909],
+        9: [0.047, 0.956],
+        10: [0.044, 1],
+    }
+
+    return closest(distribution.items(), r)
